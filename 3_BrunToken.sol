268cfd5be0785d96424a67394e1bc9725581ef42
Wbe3-Zachy-ong <zach199799@gmail.com>
2026-03-31 05:09:40 +0800

    Create BurnToken contract with ERC20 functionality

--- 3_BrunToken.sol
+++ 3_BrunToken.sol
@@ -1,1 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol"; 
+
+contract BurnToken is ERC20, ERC20Burnable {
+    constructor() ERC20("Fire Coin", "FIRE") {
+        // 给老板发 100 万个初始代币
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+}
