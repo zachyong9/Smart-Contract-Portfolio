466d8e911321d69ea724dd2b5f9d3b591a95c920
Wbe3-Zachy-ong <zach199799@gmail.com>
2026-04-06 02:07:59 +0800

    Create MyContractV1 with upgradeable functionality
    Implement an upgradeable contract with initializable and ownable features.

--- V1.sol
+++ V1.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract MyContractV1 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 public data;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers(); // 保护逻辑合约不被恶意初始化
+    }
+
+    // 替代 constructor 的初始化函数
+    function initialize(address initialOwner) initializer public {
+        __Ownable_init(initialOwner);
+        data = 100; // 初始状态数据设为 100
+    }
+
+    // UUPS 核心：授权升级，只有 Owner 可以执行
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
