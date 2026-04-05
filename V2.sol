4abe75a8cf61a5af639960d92ac1170395ceb9b9
Wbe3-Zachy-ong <zach199799@gmail.com>
2026-04-06 02:10:19 +0800

    Implement changeData function in MyContractV2
    Added changeData function to modify data value.

--- V2.sol
+++ V2.sol
@@ -1,1 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.22;
 
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+contract MyContractV2 is Initializable, UUPSUpgradeable, OwnableUpgradeable {
+    uint256 public data;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    // V2 不需要重新 initialize，状态会保留
+
+    // 新增的功能：修改 data 的值
+    function changeData(uint256 _newData) public {
+        data = _newData;
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
+}
